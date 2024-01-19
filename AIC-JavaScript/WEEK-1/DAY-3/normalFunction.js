f81c3e891dd87baad2da28ca492ef55f6f286b89
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-01-19 19:12:50 +0530

    day3 normalfunction

---
+++ AIC-JavaScript/WEEK-1/DAY-3/normalFunction.js
@@ -0,0 +1,11 @@
+function hello(name){
+    return "Hello "+name;
+}
+console.log(hello("Himanshi")); 
+//add numbers
+function addnumbers(x,y){
+    return x+y;
+}
+
+let result=addnumbers(90,89);
+console.log(result)
