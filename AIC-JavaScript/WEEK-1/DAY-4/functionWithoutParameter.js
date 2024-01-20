8c586439abd91d7d75d21136fca6369c5f054585
Himanshi <bhardwaj.himanshi2468@gmail.com>
2024-01-20 19:12:27 +0530

    Day4

---
+++ AIC-JavaScript/WEEK-1/DAY-4/functionWithoutParameter.js
@@ -0,0 +1,15 @@
+function multiply(){
+    let a=5
+    let b=9
+    return a*b;
+}
+let result=multiply();
+console.log(result);
+//Division
+function Divison(){
+    let g=5
+    let h=3
+    return g/h;
+}
+let divison = Divison();
+console.log(divison);
